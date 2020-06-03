9d0426aa3b373a503dbe7a507ec46af7c90a7dbe
Raihan Nismara <31585789+raihan71@users.noreply.github.com>
2020-06-03 14:47:55 +0700

    Update users.service.ts

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
+const API_ENDPOINT = '//jsonplaceholder.typicode.com/users';
 
 @Injectable({
   providedIn: 'root'
